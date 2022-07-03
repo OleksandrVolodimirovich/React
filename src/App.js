466e6caba5bbc248099bf6b95a0434c023ce4184
unknown <net_box@ukr.net>
2022-07-04 00:08:11 +0300

    [LES2] setState, render, оператор ..., зміна контенту при кліку, багаторазове використання {car}

--- src/App.js
+++ src/App.js
@@ -4,22 +4,27 @@ import React from "react";
 class Car extends React.Component{
   constructor(props){
     super(props);
-    this.state = { // //? це initial state(початковий стан) - дані котрі будуть автоматично в компоненті або в 
-      //  //? функції під час її ініціалізації.
+    this.state = {
       color: 'red',
       brend: 'toyota',
-      model: 'rav4'
+      model: 'rav4',
     };
   }
-  render(){
-    console.log(this.props.car);
-    console.log(this.props); //* отримаємо об'єкт {car} котрий знаходиться в фрагменті
-    
-    const year = 12; //* можна створити змінну і вивести її в тому ж методі render
+
+  handleClick(){
+    this.setState ({model: 'BMW', color: 'black'})
+  }
+
+  render(){   
+    console.log(this.state); 
     return (
       <div>
         <i>Виводимо через наслідування методу render класу Car :</i>
-        <h2>Hello, I'm Car old {year}, brend: {this.state.brend}, color: {this.state.color}</h2>  {/* //* застосування властивості з методу state */}
+        {/* //*Зазвичай bind застосовується для фіксації цього методу об'єкта */}
+        <h2 onClick = {this.handleClick.bind(this)}>
+          Hello, I'm Car {this.props.car}, model: {this.state.model}, 
+           color: {this.state.color}
+          </h2>
       </div>
     )
   }
@@ -28,6 +33,23 @@ class Car extends React.Component{
 const App = () => {
   const name = "Ira";
   const value = `${name}`;
+  const a = {
+    a: 'aa',
+    b: 'ba',
+  };
+  const b = {
+    a: 'ab',
+    c: 'cb',
+  };
+  const c = {
+    b: 'ac',
+    c: 'cc',
+  };
+  const d = {
+    ...a, ...b, ...c
+  };
+  console.log(d);
+
   const element = (
     <div>
       <i>Виводимо через основну компонету App :</i>
@@ -35,12 +57,15 @@ const App = () => {
       <h3>Hello your name is</h3> 
     </div>
   );
-  return( //* <></> - це фрагмент
+  return(
     <>
       {element} 
-      <Car car={'Mersedes'} value={name}/>  {/* //* об'єкт car котрий передається в render} */}
+      <Car car={'Mersedes'} value={name}/> 
+      <Car car={'Mersedes'} value={name}/> 
+      <Car car={'Mersedes'} value={name}/> 
+      <Car car={'Mersedes'} value={name}/> 
     </>
-  )
+  );
 };
 
 export default App;
