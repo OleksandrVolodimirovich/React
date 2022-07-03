4c7a9bca7675882b39a16b80150b0ff03d061ad7
unknown <net_box@ukr.net>
2022-07-03 21:02:09 +0300

    [LES-2] Class, props, initial state. App, class Car extends React.Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import "./App.css";
-import Header from './components/Header/Header';
-import Content from './components/Content/Content';
-import Footer from './components/Footer/Footer';
+import React from "react";
 
-
-function App() {
-const handleClick = () => {
-  console.log('Click')
+class Car extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = { // //? це initial state(початковий стан) - дані котрі будуть автоматично в компоненті або в 
+      //  //? функції під час її ініціалізації.
+      color: 'red',
+      brend: 'toyota',
+      model: 'rav4'
+    };
+  }
+  render(){
+    console.log(this.props.car);
+    console.log(this.props); //* отримаємо об'єкт {car} котрий знаходиться в фрагменті
+    
+    const year = 12; //* можна створити змінну і вивести її в тому ж методі render
+    return (
+      <div>
+        <i>Виводимо через наслідування методу render класу Car :</i>
+        <h2>Hello, I'm Car old {year}, brend: {this.state.brend}, color: {this.state.color}</h2>  {/* //* застосування властивості з методу state */}
+      </div>
+    )
+  }
 }
 
-  return (
-    <div className="App">
-      <div className="wrapper">
-        <Header/>
-        <Content/>
-        <Footer handleClick={handleClick}/>
-      </div>
+const App = () => {
+  const name = "Ira";
+  const value = `${name}`;
+  const element = (
+    <div>
+      <i>Виводимо через основну компонету App :</i>
+      <h1>Hello my name is {value}</h1>
+      <h3>Hello your name is</h3> 
     </div>
   );
-}
+  return( //* <></> - це фрагмент
+    <>
+      {element} 
+      <Car car={'Mersedes'} value={name}/>  {/* //* об'єкт car котрий передається в render} */}
+    </>
+  )
+};
 
 export default App;
---
+++ src/components/Exsample.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Exsample.css";
+
+function Exsample() {
+  return <h1>Привіт</h1>;
+}
+
+export default Exsample;
