d46377f17d91e0a85c80e472b7e9fc81c1b1fe4f
unknown <net_box@ukr.net>
2022-07-12 11:57:02 +0300

    [APP]refactor Dialogs, add key={} in map

--- src/conponents/Dialogs/Dialogs.jsx
+++ src/conponents/Dialogs/Dialogs.jsx
@@ -33,31 +33,17 @@ const Dialogs = () => {
 	];
 
 	let dialogsElements = dialogsData.map((dialog) => (
-		<DialogItem name={dialog.name} id={dialog.id} />
+		<DialogItem name={dialog.name} id={dialog.id} key={dialog.id} />
 	));
 
 	let messageElements = messagesData.map((mes) => (
-		<Message message={mes.message}  id={mes.id}/>
+		<Message message={mes.message} id={mes.id} key={mes.id} />
 	));
 
 	return (
 		<div className={s.dialogs}>
-			<div className={s.dialogsItem}>
-				{dialogsElements}
-				{/* <DialogItem name={dialogsData[0].name} id={dialogsData[0].id} />
-				<DialogItem name={dialogsData[1].name} id={dialogsData[1].id} />
-				<DialogItem name={dialogsData[2].name} id={dialogsData[2].id} />
-				<DialogItem name={dialogsData[3].name} id={dialogsData[3].id} />
-				<DialogItem name={dialogsData[4].name} id={dialogsData[4].id} />
-				<DialogItem name={dialogsData[5].name} id={dialogsData[5].id} /> */}
-			</div>
-			<div className={s.messages}>
-				{messageElements}
-				{/* <Message message={messagesData[0].message} />
-				<Message message={messagesData[1].message} />
-				<Message message={messagesData[2].message} />
-				<Message message={messagesData[3].message} /> */}
-			</div>
+			<div className={s.dialogsItem}>{dialogsElements}</div>
+			<div className={s.messages}>{messageElements}</div>
 		</div>
 	);
 };
