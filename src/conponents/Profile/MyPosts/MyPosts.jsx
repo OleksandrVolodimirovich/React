0cc85986ddfb9c612bc07b7a45bab77e3825d7ce
unknown <net_box@ukr.net>
2022-07-17 12:25:58 +0300

    [APP] createRef() для звернення до віртуал DOM

--- src/conponents/Profile/MyPosts/MyPosts.jsx
+++ src/conponents/Profile/MyPosts/MyPosts.jsx
@@ -7,17 +7,19 @@ const MyPosts = (props) => {
 		<Post id={p.id} message={p.message} like={p.likesCount} key={p.id}/>
 	));
 
-		let addPost = () => {
-			let text = document.getElementById('new-post').value;
-			alert(text)
-		}
+	let newPostElement = React.createRef();
+
+	let addPost = () => {
+		let text = newPostElement.current.value;
+		alert(text)
+	}
 
 	return (
 		<div className={s.postsBlock}>
 			<h3>my posts</h3>
 			<div>
 				<div>
-					<textarea id="new-post"></textarea>
+					<textarea ref={newPostElement}></textarea>
 				</div>
 				<div>
 					<button onClick={addPost}>Add post</button>
